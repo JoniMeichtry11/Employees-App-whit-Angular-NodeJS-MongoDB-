0b7f418fe04d3b8fdc41cd2fb5a05f06fb9c4e2e
JoniMeichtry11 <joni.meichtry@gmail.com>
2021-10-12 10:50:17 -0300

    servidor

---
+++ server/src/controllers/employees.controller.js
@@ -0,0 +1,35 @@
+const employeeCtrl = {}
+
+const Employee = require('../models/Employee')
+
+employeeCtrl.getEmployees = async (req, res) => {
+    const employees = await Employee.find()
+    res.json(employees)
+}
+employeeCtrl.createEmployee = async (req, res) => {
+    const newEmployee = new Employee(req.body)
+    try{
+        await newEmployee.save()
+    
+        res.send({message: "Employee Created"});
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+employeeCtrl.getEmployee = async (req, res) => {
+    console.log(req.params)
+    const employee = await Employee.findById(req.params.id)
+    res.send(employee);
+}
+employeeCtrl.editEmployee = async (req, res) => {
+    await Employee.findByIdAndUpdate(req.params.id, req.body)
+    res.json({status: 'employee Updated'})
+}
+
+employeeCtrl.deleteEmployee = async (req, res) => {
+    await Employee.findByIdAndDelete(req.params.id)
+    res.json({status: 'Employee Deleted'})
+}
+
+module.exports = employeeCtrl;
